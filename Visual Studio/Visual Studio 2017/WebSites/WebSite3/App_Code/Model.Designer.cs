263aba7a206e06f123ff2defcfbf34e6b86b45bc
Omer Elgoni <omer.elgoni@gmail.com>
2017-10-04 23:13:15 +0200

    Fixed a bunch of bugs related to admin status added an option to remove admin status fixed sorting and exporting for the time sheet c

--- Visual Studio/Visual Studio 2017/WebSites/WebSite3/App_Code/Model.Designer.cs
+++ Visual Studio/Visual Studio 2017/WebSites/WebSite3/App_Code/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\owere\Documents\Visual Studio 2017\WebSites\WebSite3\App_Code\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'G:\NEWER_ONE\Visual Studio\Visual Studio 2017\WebSites\WebSite3\App_Code\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
